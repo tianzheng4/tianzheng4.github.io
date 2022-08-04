e7c9d847f3e470f8d92494889175ec22b7f596f5
Tianhang Zheng <tzheng4@buffalo.edu>
2022-08-04 16:34:58 -0400

    Update hp

--- assets/js/script.js
+++ assets/js/script.js
@@ -139,6 +139,10 @@ function ResumeClick()
     window.open("./assets/cv_tianhang.pdf","_blank");
 }
 
+function PubClick() {
+  location.href='about-us.html';
+};
+
 // // page navigation variables
 // const navigationLinks = document.querySelectorAll("[data-nav-link]");
 // const pages = document.querySelectorAll("[data-page]");
